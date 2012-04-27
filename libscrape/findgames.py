9a7ea039dead188e7957f4fbc7bd00ec2b9514b1
Kenneth Pascual <kp@Kenneth-Pascuals-MacBook.local>
2012-04-27 08:30:49 -0700

    updating auto-find-games script

--- libscrape/findgames.py
+++ libscrape/findgames.py
@@ -7,6 +7,7 @@ from libscrape.config import db
 
 # A script that scrapes ESPN.com's scoreboard to retrieve ESPN's game_id and store into MySQL db
 
+dbobj = db.Db(db.dbconn_nba)
 
 def getScoreboardDoc(dt): 
     url = 'http://espn.go.com/nba/scoreboard?date=%s' % dt.isoformat().replace('-','')
@@ -30,12 +31,18 @@ def getGameIdsAndTeams(html):
     game_info = []
     for game_id in game_ids:
         team_div = soup.findAll(id='%s-awayTeamName' % game_id)
-        away_team = findTeamName(team_div[0].a.renderContents())
+        away_team_id, away_team, away_team_nbacom, away_team_nickname = findTeamName(team_div[0].a.renderContents())
 
         team_div = soup.findAll(id='%s-homeTeamName' % game_id)
-        home_team = findTeamName(team_div[0].a.renderContents())
+        home_team_id, home_team, home_team_nbacom, home_team_nickname = findTeamName(team_div[0].a.renderContents())
 
-        game_info.append({'espn_game_id':game_id, 'away_team': away_team, 'home_team': home_team})
+        game_info.append(
+            {'espn_game_id':game_id, 'away_team': away_team, 
+            'home_team': home_team, 'away_team_nbacom': away_team_nbacom, 'home_team_nbacom': home_team_nbacom,
+            'away_team_id': away_team_id, 'home_team_id': home_team_id, 
+            'away_team_nickname': away_team_nickname, 'home_team_nickname': home_team_nickname
+            }
+        )
 
     return game_info
 
@@ -45,25 +52,40 @@ def findTeamName(name):
     
     for team in team_data:
         if name == team['nickname'] or name == team['alternate_nickname']:
-            return team['code']
+            return (team['id'], team['code'],team['nbacom_code'], team['nickname'])
         else:
             pass
 
-    return 0 
+    return (0,0) 
 
 
 def _getTeamData():
-    result = db.nba_query_dict("SELECT * FROM team") 
+    result = dbobj.query_dict("SELECT * FROM team") 
     return result 
 
 
 def _fillInGameData(game_data, dt):
+    CURRENT_SEASON = '2011-2012'
+    SEASON_TYPE = 'POST'
+
     final = []
     for g in game_data:
         g['date_played'] = dt
         g['abbrev'] = '%s_%s@%s' % (dt, g['away_team'], g['home_team'])
+        g['away_team_code'] = g['away_team']
+        g['home_team_code'] = g['home_team']
+        g['nbacom_game_id'] = '%s/%s%s' % (dt.isoformat().replace('-',''), g['away_team_nbacom'], g['home_team_nbacom'])
         g['cbssports_game_id'] = g['abbrev'].replace('-','')
+        g['permalink'] = g['away_team_nickname'] + '-at-' + g['home_team_nickname'] + '-' + dt.strftime("%B-%d-%Y")
+        g['permalink'] = g['permalink'].replace('-0','-').lower()
+        g['season'] = CURRENT_SEASON
+        g['season_type'] = SEASON_TYPE
         final.append(g)
+
+        del g['away_team_nickname']
+        del g['home_team_nickname']
+        del g['away_team_nbacom']
+        del g['home_team_nbacom']
 
     return final
 
@@ -74,28 +96,16 @@ def _writeToDatabase(games, date_played):
             INSERT IGNORE INTO game (%s) VALUES (%s)
         """ % (','.join([field for field,val in sorted(g.items())]),','.join(["'%s'" % val for field,val in sorted(g.items())]))
         
-        db.nba_query(sql)
+        dbobj.query(sql)
 
-        sql_update_nbacom = """
-            UPDATE game g
-                INNER JOIN team home_team ON home_team.code = g.home_team 
-                INNER JOIN team away_team ON away_team.code = g.away_team 
-            SET nbacom_game_id = CONCAT(replace(g.date_played,'-',''),'/',away_team.nbacom_code, home_team.nbacom_code) 
-            WHERE g.date_played = '%s'   
-        """ % date_played
-        db.nba_query(sql_update_nbacom)
 
 
 def fillInAllDates():
-    current_date = datetime.date(2012,2,27)
+    dt = datetime.date(2012,4,28)
 
-    last_date = datetime.date(2012,4,26)
+    print dt
+    main(dt)
 
-    while current_date <= last_date:
-        print current_date
-        main(current_date)
-
-        current_date = current_date + datetime.timedelta(days=1)
 
 
 def main(dt):
